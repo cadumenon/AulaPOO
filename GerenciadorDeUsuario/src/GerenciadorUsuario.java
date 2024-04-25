0c5e5a89e99386e3151604a2742b08a4b5ac9312
CARLOS EDUARDO CAMPOS MENON <carlos_menon@estudante.sesisenai.org.br>
2024-04-24 21:31:10 -0300

    Atividade crud

--- GerenciadorDeUsuario/src/GerenciadorUsuario.java
+++ GerenciadorDeUsuario/src/GerenciadorUsuario.java
@@ -2,13 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GerenciadorUsuario {
-
     private List<Usuario> usuarios = new ArrayList<>();
 
-    public static String AdicionarUsuario (Usuario usuario){
-
+    public void adicionarUsuario(Usuario usuario) {
+        usuarios.add(usuario);
     }
 
+    public List<Usuario> obterTodosUsuarios() {
+        return usuarios;
+    }
 
+    public Usuario obterUsuarioPorId(String id) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getId().equals(id)) {
+                return usuario;
+            }
+        }
+        return null;
+    }
 
+    public void atualizarUsuario(String id, String novoNome, String novoEmail) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getId().equals(id)) {
+                usuario.setNome(novoNome);
+                usuario.setEmail(novoEmail);
+                return;
+            }
+        }
+    }
+
+    public void removerUsuario(String id) {
+        usuarios.removeIf(usuario -> usuario.getId().equals(id));
+    }
 }
--- GerenciadorDeUsuario/src/Main.java
+++ GerenciadorDeUsuario/src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        GerenciadorUsuario gerenciadorUsuario = new GerenciadorUsuario();
+        {
+            int escolha;
+            do {
+                System.out.println("Menu");
+                System.out.println("1-Adicionar Usuarios");
+                System.out.println("2-Listar Usuario");
+                System.out.println("3-Buscar Usuario por id");
+                System.out.println("4-Atualizar Usuario");
+                System.out.println("5-Remover Usuario");
+                System.out.println("0-Sair");
+            }
+            switch (escolha){
+
+                case 1:
+                    System.out.println("Digite o id do usuario");
+                    String id = scanner.nextLine();
+                    System.out.println("digite o nome do usuario");
+                    String nome = scanner.nextLine();
+                    System.out.println("digite o email do usuario");
+                    String email = scanner.nextLine();
+                    gerenciadorUsuario.adicionarUsuario(new Usuario(id,nome,email));
+                    System.out.println("Usuario adicionado com sucesso!");
+                    break;
+            }
+
+
+
+
+
+
+
+        }
     }
 }
